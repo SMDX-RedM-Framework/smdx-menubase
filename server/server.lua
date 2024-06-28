0a3a3bca65d630e58264a6dcdceb9f99e3814cd0
SMDX Development <emilsven2021@gmail.com>
2024-06-28 19:00:41 +0200

    Update

---
+++ server/server.lua
@@ -0,0 +1,40 @@
+local SMDXCore = exports['smdx-core']:GetSMDX()
+
+-----------------------------------------------------------------------
+-- version checker
+-----------------------------------------------------------------------
+local function versionCheckPrint(_type, log)
+    local color = _type == 'success' and '^2' or '^1'
+
+    print(('^5['..GetCurrentResourceName()..']%s %s^7'):format(color, log))
+end
+
+local function CheckVersion()
+    PerformHttpRequest('https://raw.githubusercontent.com/SMDX-RedM-Framework/smdx-menubase/main/version.txt', function(err, text, headers)
+        local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version')
+
+        if not text then 
+            versionCheckPrint('error', 'We are unable to run a version check at this moment...')
+            return 
+        end
+
+        --versionCheckPrint('success', ('Current Version: %s'):format(currentVersion))
+        --versionCheckPrint('success', ('Latest Version: %s'):format(text))
+        
+        if text == currentVersion then
+            versionCheckPrint('success', 'You are using the latest version!')
+        else
+            versionCheckPrint('error', ('You are using a old version of the script, please update to version %s'):format(text))
+        end
+    end)
+end
+
+-----------------------------------------------------------------------
+
+
+-----------------------------------------------------------------------
+
+--------------------------------------------------------------------------------------------------
+-- start version check
+--------------------------------------------------------------------------------------------------
+CheckVersion()
